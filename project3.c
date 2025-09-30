92a9e81a7d70632cf7cb4ae93af706a335a31f2d
tjdals2 <heerang1018@gmail.com>
2025-09-30 15:44:49 +0900

    Update project3.c

--- project3.c
+++ project3.c
@@ -1,164 +1,164 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-#define MAX_NODES 1000
+#define MAX_NODES 50
 
-typedef struct Node {
+typedef struct {
     char data;
     int left;
     int right;
-} Node;
-
-typedef struct Stack {
-    int items[MAX_NODES];
-    int top;
-} Stack;
-
-// 트리를 저장할 전역 배열과 노드 개수 카운터
-Node tree[MAX_NODES];
-int node_count = 1;
+} TreeNode;
 
 
-void init_stack(Stack *s) { s->top = -1; }
-void push(Stack *s, int item) { s->items[++(s->top)] = item; }
-int pop(Stack *s) { return s->items[(s->top)--]; }
-int is_empty(Stack *s) { return s->top == -1; }
+TreeNode tree[MAX_NODES];
+int node_count = 1; // 인덱스1을 루트로 저장
 
-// 새 노드를 생성하고 전역 tree 배열에 추가
-int new_node(char data) {
-    tree[node_count].data = data;
-    tree[node_count].left = -1;
-    tree[node_count].right = -1;
-    return node_count++;
+int stack[MAX_NODES];
+int top = -1;
+
+void push(int item) {
+    stack[++top] = item;
 }
 
-// 공백제거하뭇
-void trim(char *str) {
-    if (str == NULL) return;
-    char *writer = str, *reader = str;
-    while (*reader) {
-        if (!isspace((unsigned char)*reader)) {
-            *writer++ = *reader;
+int pop() {
+    return stack[top--];
+}
+
+int is_empty() {
+    return top == -1;
+}
+
+//공백제거함수
+void trim(char* str) {
+    char* write_ptr = str;
+    char* read_ptr = str;
+    while (*read_ptr) {
+        if (!isspace((unsigned char)*read_ptr)) {
+            *write_ptr++ = *read_ptr;
         }
-        reader++;
+        read_ptr++;
     }
-    *writer = '\0';
+    *write_ptr = '\0'; 
 }
 
-// 표준 (Root Left Right) 구조를 해석하는 재귀 파서
-int parse_tree(const char *str, int *pos) {
-    // 닫는 괄호거나 문자열 끝이면 자식 없음(-1)을 반환
-    if (str[*pos] == ')' || str[*pos] == '\0') {
-        if (str[*pos] == ')') (*pos)++; // 닫는 괄호는 읽고 지나감
-        return -1;
+
+// 괄호 문자열을 파싱하여 배열 트리로 구축하는 함수
+void build_tree_from_string(const char* str) {
+    int parent_stack[MAX_NODES];
+    int parent_top = -1;
+    int last_node_index = 0;
+
+    for (int i = 0; i < strlen(str); ++i) {
+        
+
+        if (isalpha(str[i])) {
+            // 현재 노드 정보 채우기
+            tree[node_count].data = str[i];
+            tree[node_count].left = 0; // 0은 NULL을 의미
+            tree[node_count].right = 0;
+
+            // 부모 스택이 비어있지 않다면, 현재 노드를 부모의 자식으로 연결
+            if (parent_top != -1) {
+                int parent_index = parent_stack[parent_top];
+                if (tree[parent_index].left == 0) {
+                    tree[parent_index].left = node_count;
+                } else {
+                    tree[parent_index].right = node_count;
+                }
+            }
+            last_node_index = node_count;
+            node_count++;
+        } else if (str[i] == '(') {
+            // '(' 앞의 노드가 새로운 부모가 됨
+            parent_stack[++parent_top] = last_node_index;
+        } else if (str[i] == ')') {
+            // 현재 서브트리 구성 완료, 부모 스택에서 pop
+            parent_top--;
+        }
     }
-
-    int root_idx = -1;
-
-    // (A ... ) 형태의 서브트리
-    if (str[*pos] == '(') {
-        (*pos)++; // '(' 건너뛰기
-        root_idx = new_node(str[*pos]); // 루트 노드 생성
-        (*pos)++;
-
-        // 재귀적으로 왼쪽과 오른쪽 자식 파싱
-        tree[root_idx].left = parse_tree(str, pos);
-        tree[root_idx].right = parse_tree(str, pos);
-
-        // 서브트리의 ')'를 찾을 때까지 건너뜀
-        if (str[*pos] == ')') (*pos)++;
-    }
-    // C, D 와 같은 단일 노드(리프 노드)
-    else if (isalpha(str[*pos])) {
-        root_idx = new_node(str[*pos]);
-        (*pos)++;
-    }
-    return root_idx;
 }
 
-// 전위 순회 
-void preorder_traversal(int root_idx) {
-    if (root_idx == -1) return;
-    Stack s;
-    init_stack(&s);
-    push(&s, root_idx);
-
+// 1. 반복적 전위 순회 (Iterative Pre-order)
+void iterative_preorder(int root_index) {
+    if (root_index == 0) return;
+    top = -1; // 스택 초기화
+    push(root_index);
     printf("pre-order: ");
-    while (!is_empty(&s)) {
-        int current_idx = pop(&s);
-        printf("%c ", tree[current_idx].data);
-        if (tree[current_idx].right != -1) push(&s, tree[current_idx].right);
-        if (tree[current_idx].left != -1) push(&s, tree[current_idx].left);
-    }
-    printf("\n");
-}
+    while (!is_empty()) {
+        int current_index = pop();
+        printf("%c ", tree[current_index].data);
 
-// 중위 순회 
-void inorder_traversal(int root_idx) {
-    if (root_idx == -1) return;
-    Stack s;
-    init_stack(&s);
-    int current_idx = root_idx;
-
-    printf("in-order: ");
-    while (current_idx != -1 || !is_empty(&s)) {
-        while (current_idx != -1) {
-            push(&s, current_idx);
-            current_idx = tree[current_idx].left;
+        if (tree[current_index].right != 0) {
+            push(tree[current_index].right);
         }
-        current_idx = pop(&s);
-        printf("%c ", tree[current_idx].data);
-        current_idx = tree[current_idx].right;
+        if (tree[current_index].left != 0) {
+            push(tree[current_index].left);
+        }
     }
     printf("\n");
 }
 
-// 후위 순회 
-void postorder_traversal(int root_idx) {
-    if (root_idx == -1) return;
-    Stack s1, s2;
-    init_stack(&s1);
-    init_stack(&s2);
-    push(&s1, root_idx);
+// 2. 반복적 중위 순회 (Iterative In-order)
+void iterative_inorder(int root_index) {
+    if (root_index == 0) return;
+    top = -1; // 스택 초기화
+    int current_index = root_index;
+    printf("in-order:  ");
+    while (current_index != 0 || !is_empty()) {
+        while (current_index != 0) {
+            push(current_index);
+            current_index = tree[current_index].left;
+        }
+        current_index = pop();
+        printf("%c ", tree[current_index].data);
+        current_index = tree[current_index].right;
+    }
+    printf("\n");
+}
 
-    while (!is_empty(&s1)) {
-        int current_idx = pop(&s1);
-        push(&s2, current_idx);
-        if (tree[current_idx].left != -1) push(&s1, tree[current_idx].left);
-        if (tree[current_idx].right != -1) push(&s1, tree[current_idx].right);
+// 3. 반복적 후위 순회 (Iterative Post-order)
+void iterative_postorder(int root_index) {
+    if (root_index == 0) return;
+    int stack2[MAX_NODES];
+    int top2 = -1;
+    
+    top = -1; 
+    push(root_index);
+    
+    while (!is_empty()) {
+        int current_index = pop();
+        stack2[++top2] = current_index;
+
+        if (tree[current_index].left != 0) {
+            push(tree[current_index].left);
+        }
+        if (tree[current_index].right != 0) {
+            push(tree[current_index].right);
+        }
     }
 
     printf("post-order: ");
-    while (!is_empty(&s2)) {
-        printf("%c ", tree[pop(&s2)].data);
+    while (top2 != -1) {
+        printf("%c ", tree[stack2[top2--]].data);
     }
     printf("\n");
 }
 
-int main(void) {
-    char buf[MAX_NODES];
+int main() {
+    char input[256]; // 사용자 입력을 저장할 버퍼
 
     printf("input: ");
-    scanf("%[^\n]", buf);
-    //(A (B C D) (E G (H I (J (K L) (M N))))) 입력
+    scanf(" %[^\n]", input);
 
-    trim(buf); 
+    trim(input);
 
-    int pos = 0;
-    int root_idx = parse_tree(buf, &pos); // 문자열을 해석해 트리 생성
+    build_tree_from_string(input);
 
-    // 생성된 트리를 순회하며 결과 출력
-    if (root_idx != -1) {
-        preorder_traversal(root_idx);
-        //A B C D E G H I J K L M N 출력
-        inorder_traversal(root_idx);
-        //C B D A G E I H L K J N M 출력
-        postorder_traversal(root_idx);
-        //C D B G I L K N M J H E A 출력
-    }
+    iterative_preorder(1);  
+    iterative_inorder(1);
+    iterative_postorder(1);
 
     return 0;
 }
-
